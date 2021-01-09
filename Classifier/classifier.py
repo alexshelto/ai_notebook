4eac72c386455efd143dc449bb80f33601fd8b65
Alex Shelton <alexandershelton@icloud.com>
2021-01-08 20:14:16 -0500

    cost function work

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -5,14 +5,13 @@ from scipy import optimize
 '''plotting/showing data'''
 import matplotlib.pyplot as plt
 
-#todo:
-# K and L
-# input layer size, hidden layer size
+
+# input layer size, num layers
 class Classifier:
-    def __init__(self, dataset, epsilon=1.7):
+    def __init__(self, dataset, hidden_layer_size=25, epsilon=1.7):
         print('Loading data set')
         self.dataset = np.genfromtxt(dataset, delimiter=',')
-        self.y = self.dataset[:, 0]  # labels 
+        self.y = self.dataset[:, 0].reshape(-1,1)  # labels 
         self.X = self.dataset[:, 1:] # Input data
         self.m =  len(self.y)     # number of training examples 
         self.lam = 0 # lambda
@@ -24,10 +23,11 @@ class Classifier:
         # creating theta params. the (+1) in the thetas is to account for fitting the bias
         self.theta1 = np.random.rand(self.hidden_layer_size, self.input_layer_size + 1) # theta for calculating L=2. default 25 units in layer 2
         self.theta2 = np.random.rand(self.hidden_layer_size + 1, self.num_labels)         # theta for output layer. default 25 + 1 units, 26 total classes a-z
+        # creating a flattened array of theta values for the optimization function
         self.params = np.r_[self.theta1.T.flatten(), self.theta2.T.flatten()]
 
     def show(self, x, name):
-        print(f'Size of {name}: {np.shape(x)}')
+        print(f'size of {name}: {np.shape(x)}')
 
     def sigmoid(self, z):
         return ( (1 / (1 + np.exp(-z))) )
@@ -50,12 +50,25 @@ class Classifier:
         a3 = self.sigmoid(z3) # a3 = h_theta(x)
         return (a1, a2, a3, z2, z3)
 
-
-
-
     def cost_function(self):
         # FWD Propogate
         a1, a2, a3, z2, z3 = self.feed_forward()
-        # Compute Cost
-        J = 
-        # Back Propogate
+        # Compute Cost, note: a3 = h_theta(x)
+        
+        # create a matrix, (num of training ex, num of labels)
+        # 0's else, 1 on the correct index corresponding to the label. ie: for letter a: put a 1 on the 0th index, if letter is z: 25th index
+        y_k = np.zeros((self.m, self.num_labels))
+        for i in range(0, len(self.y)):
+            label_index = y[i]
+            y_k[i, label_index] = 1
+        
+        inner_term0 = (-y_k * np.log(a3))
+        inner_term1 = (1 - yk) * np.log(1-a3)
+        left_side = np.sum(inner_term0 + inner_term1) / self.m
+
+
+        #back propogate
+
+
+
+
